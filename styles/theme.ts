f34da73ec7165108d8852e752848c53d12c2b767
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-11-12 12:20:01 -0300

    feat: refact system design of project

---
+++ styles/theme.ts
@@ -0,0 +1,33 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#181B23",
+      "800": "#1F2029",
+      "700": "#353646",
+      "600": "#464858",
+      "500": "#616480",
+      "400": "#767885",
+      "300": "#9699B0",
+      "200": "#B3B5C6",
+      "100": "#D1D2DC",
+      "50": "#EEEEF2",
+    },
+    purple: {
+      "500": "#7D5AD8",
+    },
+  },
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.900", // background
+        color: "gray.50",
+      },
+    },
+  },
+});
