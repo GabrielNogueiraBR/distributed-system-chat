d00c69350035e070827ccf123eefce7f69c40f76
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-11-12 16:06:53 -0300

    feat: updating chat form

--- src/components/ChatForm/index.tsx
+++ src/components/ChatForm/index.tsx
@@ -1,15 +1,59 @@
 import { Button, HStack } from "@chakra-ui/react";
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { TextArea } from "../Form/TextArea";
 
+import * as yup from "yup";
+
+const formMessageSchema = yup.object().shape({
+  message: yup.string().required(),
+});
+
 const ChatForm = () => {
+  const [message, setMessage] = useState<string>();
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const handleMessageChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value;
+    setMessage(value);
+  };
+
+  const handleOnSubmitForm = async (event: React.FormEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const data = { message };
+
+    const isValid = await formMessageSchema.isValid(data);
+
+    if (!isValid) {
+      setIsError(true);
+      return;
+    }
+
+    //TODO: CRIAR LOGICA PARA ENVIAR NOVA MENSAGEM AO SOCKET
+    //TODO: CRIAR CONTEXT PARA CONEXAO COM O SOCKET PARA COMPARTILHAR O SOCKET ENTRE VARIAS TELAS
+  };
+
+  useEffect(() => {
+    setIsError(false);
+  }, [message]);
+
   return (
-    <HStack flex="1" mt="8" justify={"flex-end"} spacing="4">
+    <HStack
+      as="form"
+      flex="1"
+      mt="8"
+      justify={"flex-end"}
+      spacing="4"
+      onSubmit={handleOnSubmitForm}
+    >
       <TextArea
         name="message-input"
-        placeholder="Here is a sample placeholder"
+        placeholder="Digite uma mensagem..."
         size="md"
         resize="none"
+        value={message}
+        onChange={handleMessageChange}
+        isInvalid={isError}
+        errorMessage={"Formulário inválido"}
       />
       <Button type="submit" colorScheme={"pink"} px="12" py="2.5">
         Enviar
