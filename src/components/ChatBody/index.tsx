b79c6b181e043caa282e6d8275207721d12d7b7b
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-11-14 22:24:59 -0300

    feat: adding scroll to bottom on chat

--- src/components/ChatBody/index.tsx
+++ src/components/ChatBody/index.tsx
@@ -1,5 +1,5 @@
 import { VStack } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useChat } from "../../context/ChatContext";
 import { useLogin } from "../../context/LoginContext";
 import ChatMessage from "../ChatMessage";
@@ -8,8 +8,21 @@ const ChatBody = () => {
   const { login } = useLogin();
   const { messages } = useChat();
 
+  const chatRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    chatRef.current?.scrollTo({
+      behavior: "smooth",
+      top: chatRef.current.scrollHeight,
+    });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <VStack w="100%" maxH="24rem" minH="24rem" overflowY="auto">
+    <VStack ref={chatRef} w="100%" maxH="24rem" minH="24rem" overflowY="auto">
       {messages.map((message, index) => {
         return (
           <ChatMessage
