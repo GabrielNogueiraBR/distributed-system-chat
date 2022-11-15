45e29350f3094e6a09640848cd8489106cb9674f
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-11-14 21:22:46 -0300

    refact: mapping messages

--- src/components/ChatBody/index.tsx
+++ src/components/ChatBody/index.tsx
@@ -1,25 +1,24 @@
 import { VStack } from "@chakra-ui/react";
 import React from "react";
-import Message from "../../types/Message";
+import { useChat } from "../../context/ChatContext";
+import { useLogin } from "../../context/LoginContext";
 import ChatMessage from "../ChatMessage";
 
-const message: Message = {
-  author: "Fulano",
-  content: "teste",
-};
-
 const ChatBody = () => {
+  const { login } = useLogin();
+  const { messages } = useChat();
+
   return (
-    <VStack w="100%" maxH="24rem" overflowY="auto">
-      <ChatMessage message={message} />
-      <ChatMessage message={message} align="right" />
-      <ChatMessage message={message} />
-      <ChatMessage message={message} isInitial={false} />
-      <ChatMessage message={message} isInitial={false} />
-      <ChatMessage message={message} isInitial={false} />
-      <ChatMessage message={message} isInitial={false} />
-      <ChatMessage message={message} isInitial={false} />
-      <ChatMessage message={message} isInitial={false} />
+    <VStack w="100%" maxH="24rem" minH="24rem" overflowY="auto">
+      {messages.map((message, index) => {
+        return (
+          <ChatMessage
+            key={index}
+            message={message}
+            isUserAuthor={login === message.author}
+          />
+        );
+      })}
     </VStack>
   );
 };
